1be202e3ca625f5ec74330a0a3498a88c1a5ce93
tomascarlsson <tomas.tc@gmail.com>
2012-10-20 13:21:20 +0200

    Original kod
    Detta är originalkoden

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="sv">
+<head>
+    <title>TITEL</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<?php 
+	// condition ? value if true : value if false;
+	$i = 'sant';
+	$j = 'falskt';
+
+	echo $result = 10 > 12 ? $i : $j;
+
+
+ ?>
+</body>
+</html>
+
+
+
